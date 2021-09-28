1020520ebb983d24d83a470e69793e0a978fa758
Eduardo Piña <mrejpm@gmail.com>
2021-09-28 18:00:00 -0400

    :lipstick: Add contact page and basic functions

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -44,16 +44,20 @@ const Header : React.FC = () => {
     }
   }
 
+
   const headerOptions = setHeaderOptions(router.pathname);
+
+  const leftColor = router.query.type==='web'?'blue':'white'
+  const rightColor = router.query.type==='iot' || router.pathname === '/contact'?'white':'blue'
 
   return (
     <header className={styles.header}>
-      <div className={styles.header__left}>
+      <div className={`${styles.header__left} ${styles[leftColor]} `}>
         <Link href="/" >
           <span className={styles.title}>Eduardo Piña</span>
         </Link>
       </div>
-      <div className={styles.header__right}>
+      <div className={`${styles.header__right} ${styles[rightColor]}`}>
         {headerOptions.map((option, index) =>
           <Link key={index} href={option.route}>
             <span className={styles.header__item}>{option.name}</span>
--- pages/contact/index.tsx
+++ pages/contact/index.tsx
@@ -1,8 +1,66 @@
-import React from 'react';
+import React, {useState} from 'react';
+import Button from '../../components/Button/Button';
+import styles from './Contact.module.css';
+import github from '../../public/images/github.svg'
+import twitter from '../../public/images/twitter.svg'
+import linkedin from '../../public/images/linkedin.svg'
+import instagram from '../../public/images/instagram.svg'
+import Image from 'next/image';
 
 const Contact = () => {
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+  const [name, setName] = useState('');
+  const [subject,setSubject] = useState('');
+
+  const sendMessage = () => {
+    console.log(email, message, name, subject);
+  }
+  const clearInputs = () => {
+    setEmail('');
+    setMessage('');
+    setName('');
+    setSubject('');
+  }
+
   return (
-    <h1>Contact</h1>
+    <div className={styles.contactMe}>
+      <h1>Contact Me</h1>
+      <input type="text" placeholder='NAME' value={name} onChange={(e)=>setName(e.target.value)}/>
+      <input type="text" placeholder='EMAIL' value={email} onChange={(e)=>setEmail(e.target.value)}/>
+      <input type="text" placeholder='SUBJECT' value={subject} onChange={(e)=>setSubject(e.target.value)}/>
+      <textarea placeholder='MESSAGE' value={message} onChange={(e)=>setMessage(e.target.value)}/>
+      <div className={styles.buttonContainer}>
+        <Button
+          type='primary'
+          color='blue'
+          onClick={sendMessage}
+        >
+          Send
+        </Button>
+        <Button
+          type='secondary'
+          color='blue'
+          onClick={clearInputs}
+          >
+          Clear
+        </Button>
+      </div>
+      <div className={styles.socialContainer}>
+        <a href="https://github.com/edjopima" target="_blank" >
+          <Image src={github} width={40} height={40}/>
+        </a>
+        <a href="https://twitter.com/edjopima" target="_blank" >
+          <Image src={twitter} width={40} height={40}/>
+        </a>
+        <a href="https://www.linkedin.com/in/edjopima/" target="_blank" >
+          <Image src={linkedin} width={40} height={40}/>
+        </a>
+        <a href="https://www.instagram.com/edjopima/" target="_blank" >
+          <Image src={instagram} width={40} height={40}/>
+        </a>
+      </div>
+    </div>
   );
 }
 
